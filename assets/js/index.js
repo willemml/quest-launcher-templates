b7aa6db178ca4c4997d08640768e4e9131945384
Willem Leitso <willem@leit.so>
2019-06-26 18:19:55 +0200

    added unsorted list that creates a list of all packages from a text file containing the package names one by line

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,29 @@
 var vrPackages = []
 var twoDPackages = []
-var tools = []
+var legacyVrApps = []
+var sysutils = []
+var unsorted = []
 var vrPackagesHTML = []
 var twoDPackagesHTML = []
-var toolsHTML = []
+var legacyVrAppsHTML = []
+var sysutilsHTML = []
+var unsortedHTML = []
 var vrBtn = document.getElementById('vrbtn')
+var legacyBtn = document.getElementById('legvrbtn')
 var twoDBtn = document.getElementById('2dbtn')
-var toolsBtn = document.getElementById('toolsbtn')
+var sysutilsBtn = document.getElementById('sysutilsbtn')
+var unsortedBtn = document.getElementById('unsortedbtn')
 var vrAppArea = document.getElementById('vrapps')
+var legacyVrAppArea = document.getElementById('legvrapps')
 var twoDAppArea = document.getElementById('2dapps')
-var toolsArea = document.getElementById('tools')
+var sysutilsArea = document.getElementById('sysutils')
+var unsortedArea = document.getElementById('unsorted')
 var activeButtonClass = 'btn btn-secondary active'
 var inactiveButtonClass = 'btn btn-secondary'
 var hiddenAppAreaClass = 'hidden'
 var visibleAppAreaClass = 'full-height text-center flex-row row'
-var appOpenLinkStart = '<a href="autotoolscommand://openapp=:='
+var appOpenLinkStart = '<a class="btn btn-link" href="autosysutilscommand://openapp=:='
+var fileinput = document.getElementById('textListInput')
 
 vrPackages = [
   'com.beatgames.beatsaber', 'beat-saber.png', 'Beat Saber',
@@ -28,38 +37,31 @@ twoDPackages = [
   'com.discord', 'discord.png', 'Discord'
 ]
 
-tools = [
+sysutils = [
   'com.android.settings', 'settings.png', 'Settings',
   'net.dinglisch.android.taskerm', 'tasker.png', 'Tasker'
 ]
 
-function showVr() {
-  vrBtn.className = activeButtonClass
-  twoDBtn.className = inactiveButtonClass
-  toolsBtn.className = inactiveButtonClass
-  vrAppArea.className = visibleAppAreaClass
-  twoDAppArea.className = hiddenAppAreaClass
-  toolsArea.className = hiddenAppAreaClass
-}
+fileinput.addEventListener("change", function() {
+  if (this.files && this.files[0]) {
+    var myFile = this.files[0];
+    var reader = new FileReader();
+    reader.addEventListener('load', function(e) {
+      var textByLine = e.target.result.split("\n")
+      for (var i = 0; i < textByLine.length; i++) {
+        var pNameSplit = textByLine[i].split('.')
+        var unsortedName = pNameSplit[pNameSplit.length - 1]
+        document.getElementById('unsortedlist').innerHTML += appOpenLinkStart + textByLine[i] + '"><p>' + unsortedName + '</p></a>'
+      }
+    });
+    reader.readAsBinaryString(myFile);
+  }
+})
 
-showVr()
-
-function show2D() {
-  twoDBtn.className = activeButtonClass
-  vrBtn.className = inactiveButtonClass
-  toolsBtn.className = inactiveButtonClass
-  twoDAppArea.className = visibleAppAreaClass
-  vrAppArea.className = hiddenAppAreaClass
-  toolsArea.className = hiddenAppAreaClass
-}
-
-function showTools() {
-  toolsBtn.className = activeButtonClass
-  twoDBtn.className = inactiveButtonClass
-  vrBtn.className = inactiveButtonClass
-  toolsArea.className = visibleAppAreaClass
-  twoDAppArea.className = hiddenAppAreaClass
-  vrAppArea.className = hiddenAppAreaClass
+function hideDiv(divs) {
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].className = hiddenAppAreaClass
+  }
 }
 
 function packageListsToHTML() {
@@ -87,33 +89,89 @@ function packageListsToHTML() {
     i++
     i++
   }
-  for (var i = 0; i < tools.length; i++) {
-    if (tools[i + 1] == '') {
-      tools[i + 1] = 'notfound.png'
+  for (var i = 0; i < sysutils.length; i++) {
+    if (sysutils[i + 1] == '') {
+      sysutils[i + 1] = 'notfound.png'
     }
     if (vrPackages[i + 2] == '') {
-      var pNameSplit = tools[i].split('.')
-      tools[i + 2] = pNameSplit[pNameSplit.length - 1]
+      var pNameSplit = sysutils[i].split('.')
+      sysutils[i + 2] = pNameSplit[pNameSplit.length - 1]
     }
-    toolsHTML.push(appOpenLinkStart + tools[i] + '"><img style="width:150px" src="assets/app-icons/tools/' + tools[i + 1] + '" /><p>' + tools[i + 2] + '</p></a>')
+    sysutilsHTML.push(appOpenLinkStart + sysutils[i] + '"><img style="width:150px" src="assets/app-icons/sysutils/' + sysutils[i + 1] + '" /><p>' + sysutils[i + 2] + '</p></a>')
     i++
     i++
   }
 }
 packageListsToHTML()
 
+function deactivateButton(btns) {
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].classname = inactiveButtonClass
+  }
+}
+
+function showVr() {
+  vrBtn.className = activeButtonClass
+  vrAppArea.className = visibleAppAreaClass
+  var deactbtn = [sysutilsBtn, legacyBtn, twoDBtn, unsortedBtn]
+  var deactareas = [sysutilsArea, legacyVrAppArea, twoDAppArea, unsortedArea]
+  deactivateButton(deactbtn)
+  hideDiv(deactareas)
+}
+showVr()
+
+function show2D() {
+  twoDBtn.className = activeButtonClass
+  twoDAppArea.className = visibleAppAreaClass
+  var deactbtn = [sysutilsBtn, legacyBtn, vrBtn, unsortedBtn]
+  var deactareas = [sysutilsArea, legacyVrAppArea, vrAppArea, unsortedArea]
+  deactivateButton(deactbtn)
+  hideDiv(deactareas)
+}
+
+function showLegVR() {
+  legacyBtn.className = activeButtonClass
+  legacyVrAppArea.className = visibleAppAreaClass
+  var deactbtn = [sysutilsBtn, vrBtn, twoDBtn, unsortedBtn]
+  var deactareas = [sysutilsArea, vrAppArea, twoDAppArea, unsortedArea]
+  deactivateButton(deactbtn)
+  hideDiv(deactareas)
+}
+
+function showsysutils() {
+  sysutilsBtn.className = activeButtonClass
+  sysutilsArea.className = visibleAppAreaClass
+  var deactbtn = [vrBtn, legacyBtn, twoDBtn, unsortedBtn]
+  var deactareas = [vrAppArea, legacyVrAppArea, twoDAppArea, unsortedArea]
+  deactivateButton(deactbtn)
+  hideDiv(deactareas)
+}
+
+function showUnsorted() {
+  unsortedBtn.className = activeButtonClass
+  unsortedArea.className = visibleAppAreaClass
+  var deactbtn = [sysutilsBtn, legacyBtn, twoDBtn, vrBtn]
+  var deactareas = [sysutilsArea, legacyVrAppArea, twoDAppArea, vrAppArea]
+  deactivateButton(deactbtn)
+  hideDiv(deactareas)
+}
+
 function htmlListsCreate() {
   vrAppArea.innerHTML = ''
   twoDAppArea.innerHTML = ''
-  toolsArea.innerHTML = ''
+  sysutilsArea.innerHTML = ''
+  legacyVrAppArea.innerHTML = ''
   for (var i = 0; i < vrPackagesHTML.length; i++) {
     vrAppArea.innerHTML += vrPackagesHTML[i]
   }
   for (var i = 0; i < twoDPackagesHTML.length; i++) {
     twoDAppArea.innerHTML += twoDPackagesHTML[i]
   }
-  for (var i = 0; i < toolsHTML.length; i++) {
-    toolsArea.innerHTML += toolsHTML[i]
+  for (var i = 0; i < legacyVrAppsHTML.length; i++) {
+    legacyVrAppArea.innerHTML += legacyVrAppsHTML[i]
+  }
+  for (var i = 0; i < sysutilsHTML.length; i++) {
+    sysutilsArea.innerHTML += sysutilsHTML[i]
   }
 }
 htmlListsCreate()
